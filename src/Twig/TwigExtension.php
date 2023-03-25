6091b005fc2fefdcf8dfacdd9b8ae6824817cffc
Lazare Fortune <lazarefortune@gmail.com>
2023-03-25 21:41:49 +0100

    feat: update menu_active function to check if menu item is active

--- src/Twig/TwigExtension.php
+++ src/Twig/TwigExtension.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Twig;
 
+use Symfony\Component\Routing\RouterInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
@@ -15,29 +16,21 @@ class TwigExtension extends AbstractExtension
         ];
     }
 
-    public function showIcon(string $icon, ?string $size = null): string
+    public function showIcon(string $icon, ?string $size = null, ?string $attrs = null): string
     {
         $attributes = '';
         if ($size) {
             $attributes = 'la-'. $size;
         }
 
+        if ($attrs) {
+            $attributes .= ' ' . $attrs;
+        }
+
         return <<<HTML
             <i class="las la-{$icon}  {$attributes}"></i>
         HTML;
     }
-
-//    public function showIcon(string $icon, ?int $size = null): string
-//    {
-//        $attributes = '';
-//        if ($size) {
-//            $attributes = ' style="font-size: ' . $size . 'px;"';
-//        }
-//
-//        return <<<HTML
-//            <i class="las la-{$icon}" {$attributes}></i>
-//        HTML;
-//    }
 
     /**
      * Ajout une class active pour les éléments actifs du menu.
@@ -46,9 +39,10 @@ class TwigExtension extends AbstractExtension
     public function menuActive(array $context, string $route): string
     {
         $active = '';
-        dump($context);
-        dump($route);
-        if (isset($context['_route']) && $context['_route'] === $route) {
+        $request = $context['app']->getRequest();
+        $currentRoute = $request->get('_route');
+
+        if ($currentRoute === $route) {
             $active = 'active';
         }
 
