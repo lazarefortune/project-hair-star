edc5e5f4124932841b5bec81437e9b0a1ff49ae1
Lazare Fortune <lazarefortune@gmail.com>
2023-04-08 23:09:40 +0200

    style: add register style form v1 - change auth routes urls

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -28,7 +28,7 @@ class RegistrationController extends AbstractController
         $this->emailVerifier = $emailVerifier;
     }
 
-    #[Route('/register', name: 'app_register')]
+    #[Route('/inscription', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
