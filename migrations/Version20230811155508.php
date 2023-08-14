c8fef09420863a3dfef81b0bf633bdfc4debd4bc
Lazare Fortune <lazarefortune@gmail.com>
2023-08-14 22:35:19 +0200

    update category_service, add isActive

---
+++ migrations/Version20230811155508.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230811155508 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category_service ADD is_active TINYINT(1) NOT NULL, ADD description LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category_service DROP is_active, DROP description');
+    }
+}
