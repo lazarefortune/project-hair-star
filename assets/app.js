fb7cc699f068f979fe62037f1f59f172aa07a807
Lazare Fortune <lazarefortune@gmail.com>
2024-01-05 02:39:25 +0100

    feat: add scroll top option

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,11 @@ import {createIcons, icons} from 'lucide';
 
 createIcons({icons});
 
+/* Elements */
+import {ScrollTop} from 'headless-elements'
+
+customElements.define('scroll-top', ScrollTop)
+
 /* Modules */
 import './modules/header.js'
 import './modules/scrollreveal.js'
--- assets/elements/index.js
+++ assets/elements/index.js
@@ -1,5 +1,6 @@
-import {NavTabs} from 'headless-elements'
+import {NavTabs, ScrollTop} from 'headless-elements'
 import {Spotlight} from './admin/spotlight'
 
 customElements.define('spotlight-bar', Spotlight)
 customElements.define('nav-tabs', NavTabs)
+customElements.define('scroll-top', ScrollTop)
