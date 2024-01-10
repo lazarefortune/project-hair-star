68b581b807279391526dbaf071284ba028f856a5
Lazare Fortune <lazarefortune@gmail.com>
2024-01-10 21:59:24 +0100

    feat: add feature to send email after contact form

--- src/Domain/Contact/ContactService.php
+++ src/Domain/Contact/ContactService.php
@@ -3,20 +3,36 @@
 namespace App\Domain\Contact;
 
 use App\Domain\Contact\Dto\ContactData;
+use App\Infrastructure\Mailing\MailService;
 
 class ContactService
 {
 
-    public function __construct()
+    public function __construct(
+        private readonly MailService $mailService,
+    )
     {
     }
 
+    /**
+     * Send contact message
+     * @param ContactData $contactDto
+     * @return void
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
     public function sendContactMessage( ContactData $contactDto ) : void
     {
-        // TODO: Send email
+        $contactEmail = $this->mailService->prepareEmail(
+            $contactDto->email,
+            'Demande de contact: ' . $contactDto->subject,
+            'mails/contact/contact-message.twig', [
+            'name' => $contactDto->name,
+            'message' => $contactDto->message,
+        ] );
 
-
-        // TODO: Save contact message in database
+        $this->mailService->send( $contactEmail );
     }
 
 }
