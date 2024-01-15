09995324b4109239161cf347c87e70fff006a2c7
Lazare Fortune <lazarefortune@gmail.com>
2024-01-15 21:55:32 +0100

    chore: no appointment found page

--- src/Http/Controller/AppointmentController.php
+++ src/Http/Controller/AppointmentController.php
@@ -28,10 +28,10 @@ class AppointmentController extends AbstractController
     {
         $appointment = $this->getAppointmentOrNull( $token );
 
-        if ( !$appointment ) {
-            $this->addFlash( 'danger', 'La réservation n\'existe pas' );
-            return $this->render( 'pages/message.html.twig' );
-        }
+//        if ( !$appointment ) {
+//            $this->addFlash( 'danger', 'La réservation n\'existe pas' );
+//            return $this->render( 'pages/message.html.twig' );
+//        }
 
         return $this->render( 'appointment/manage-view.html.twig', [
             'appointment' => $appointment,
